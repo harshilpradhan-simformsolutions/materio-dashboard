996a7ca6d676dda2dce733ca4c1c944d71c7ed67
Harshil Pradhan <harshilpradhan@simformsolutions.com>
2023-08-03 22:46:37 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <>
+      <Button type="primary" danger>
+        Click
+      </Button>
+    </>
+  );
+}
+
+export default App;
